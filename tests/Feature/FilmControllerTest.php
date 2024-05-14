6fb4d0fa7a8837882ae684f3b602aae47e319fd9
salaheddine es-sbai <es.sbai.ma@gmail.com>
2024-05-14 19:50:33 +0100

    PHPUnit test, FilmControllerTest testUpdate

--- tests/Feature/FilmControllerTest.php
+++ tests/Feature/FilmControllerTest.php
@@ -81,6 +81,67 @@ class FilmControllerTest extends TestCase
         $this->assertEquals($genresCount, $uniqueGenreIdsCount);
     }
     
+       /**
+     * Affiche le formulaire de modification d'un film.
+     *
+     */
     
-    
+    public function testEdit()
+    {
+        // Create a user
+        $user = User::factory()->create();
+
+        // Creating a film instance
+        $film = Film::factory()->create();
+
+        // Generating URL for edit route
+        $editUrl = route('film.edit', ['id' => $film->id]);
+
+        // Acting as a user
+        $response = $this->actingAs($user)->get($editUrl);
+
+        // Asserting the response status is 200 (OK)
+        $response->assertStatus(200);
+
+        // Asserting the response contains the film data
+        $response->assertSee($film->title)
+                 ->assertSee($film->description)
+                 ->assertSee($film->image_url);
+    }
+
+     /**
+     * Met à jour les informations d'un film.
+     *
+     */
+
+    public function testUpdate()
+    {
+        // Create a user
+        $user = User::factory()->create();
+
+        // Create a film
+        $film = Film::factory()->create();
+
+        // New data to update the film
+        $newData = [
+            'title' => 'New Title',
+            'description' => 'New Description',
+            'image_url' => 'https://example.com/new_image.jpg',
+        ];
+
+        // Generating URL for update route
+        $updateUrl = route('film.update', ['id' => $film->id]); // Adjusted route name
+
+        // Acting as a user and sending a PUT request with new data
+        $response = $this->actingAs($user)->put($updateUrl, $newData); // Used put instead of post
+
+        // Asserting the response redirects to the films index route
+        $response->assertRedirect(route('films.index'));
+
+        // Asserting the film data is updated in the database
+        $updatedFilm = Film::find($film->id);
+        $this->assertEquals($newData['title'], $updatedFilm->title);
+        $this->assertEquals($newData['description'], $updatedFilm->description);
+        $this->assertEquals($newData['image_url'], $updatedFilm->image_url);
+    }
 }
