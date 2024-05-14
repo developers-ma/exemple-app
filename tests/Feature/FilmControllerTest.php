f2a7bd6fd6679d53afc0d8ab5a5f062a7beb8f02
salaheddine es-sbai <es.sbai.ma@gmail.com>
2024-05-14 19:39:55 +0100

    PHPUnit test, fetchAndStore function

--- tests/Feature/FilmControllerTest.php
+++ tests/Feature/FilmControllerTest.php
@@ -15,6 +15,10 @@ class FilmControllerTest extends TestCase
 {
     use RefreshDatabase;
 
+      /**
+     * Affiche les détails d'un film.
+     *
+     */
     public function testDetails()
     {
         // Créer un utilisateur de test
@@ -36,4 +40,47 @@ class FilmControllerTest extends TestCase
         $response->assertSee($film->title);
         $response->assertSee($film->description);
     }
+
+     /**
+     * Récupère les films populaires depuis l'API et les enregistre en base de données.
+     *
+     */
+    public function testFetchAndStoreWithAuth()
+    {
+        // Créer un utilisateur et s'authentifier
+        $user = User::factory()->create();
+        $this->actingAs($user);
+    
+        // Vider les tables 'films' et 'genres' avant d'exécuter le test
+        Film::truncate();
+        Genre::truncate();
+    
+        // Exécuter la méthode fetchAndStore
+        $response = $this->get(route('films.fetchAndStore'));
+    
+        // Vérifier que la réponse redirige
+        $response->assertRedirect(route('films.index'));
+    
+        // Vérifier que le message de succès est présent
+        $response->assertSessionHas('success', 'Films récupérés depuis l\'API et enregistrés avec succès.');
+    
+        // Vérifier que les films sont enregistrés dans la base de données avec des movie_id uniques
+        $filmsCount = Film::count();
+        $uniqueMovieIdsCount = Film::distinct('movie_id')->count();
+    
+        // Affirmer que le nombre total de films est égal au nombre de movie_ids uniques
+        $this->assertEquals($filmsCount, $uniqueMovieIdsCount);
+    
+        // Vérifier que les genres sont enregistrés dans la base de données
+        $genresCount = Genre::count();
+    
+        //  vérifier s'il existe des entrées genre_id en double
+        $uniqueGenreIdsCount = Genre::distinct('genre_id')->count();
+    
+        // Affirmer que le nombre total de genres est égal au nombre de genre_ids uniques
+        $this->assertEquals($genresCount, $uniqueGenreIdsCount);
+    }
+    
+    
+    
 }
