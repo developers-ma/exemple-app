5ae1d00eed0d8b5ccdd8500d0842ee6d663c36cc
salaheddine es-sbai <es.sbai.ma@gmail.com>
2024-05-15 13:33:01 +0100

    optimisation des migrations

--- database/migrations/2024_05_10_184227_create_genres_table.php
+++ database/migrations/2024_05_10_184227_create_genres_table.php
@@ -10,7 +10,7 @@ class CreateGenresTable extends Migration
     {
         Schema::create('genres', function (Blueprint $table) {
             $table->id();
-            $table->string('genre_id');
+            $table->unsignedBigInteger('genre_id')->unique();
             $table->string('name');
             $table->timestamps();
         });
