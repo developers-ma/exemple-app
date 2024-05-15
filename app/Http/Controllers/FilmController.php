4d8df93e09b01b1df7bc67a72062d6c399901d89
salaheddine es-sbai <es.sbai.ma@gmail.com>
2024-05-15 22:57:53 +0100

    optimisation du code

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -44,49 +44,57 @@ class FilmController extends Controller
      */
     public function fetchAndStore(FilmRequest $filmRequest, GenreRequest $genreRequest, TMDBService $tmdbService): \Illuminate\Http\RedirectResponse|bool
 {
-    // Valider les données des films
-    $validatedFilmData = $filmRequest->validated();
+    try {
+        // Valider les données des films
+        $validatedFilmData = $filmRequest->validated();
 
-    // Valider les données des genres
-    $validatedGenreData = $genreRequest->validated();
+        // Valider les données des genres
+        $validatedGenreData = $genreRequest->validated();
 
-    // Récupérer les films populaires depuis l'API TMDB
-    $films = $tmdbService->getTrendingMovies();
+        // Récupérer les films populaires depuis l'API TMDB
+        $films = $tmdbService->getTrendingMovies();
 
-    // Enregistrer les films récupérés depuis l'API dans la base de données
-    foreach ($films as $film) {
-        $newMovie = Film::updateOrCreate(
-            ['movie_id' => $film['movie_id']], // Utilisation de la clé 'movie_id' pour vérifier l'existence du film
-            [
-                'title' => $film['title'],
-                'description' => $film['description'],
-                'image_url' => $film['image_url'],
-                'genre_ids' => $film['genre_ids']
-            ] + $validatedFilmData // Fusion des données validées avec les données de l'API
-        );
+        // Enregistrer les films récupérés depuis l'API dans la base de données
+        foreach ($films as $film) {
+            $newMovie = Film::updateOrCreate(
+                ['movie_id' => $film['movie_id']], // Utilisation de la clé 'movie_id' pour vérifier l'existence du film
+                [
+                    'title' => $film['title'],
+                    'description' => $film['description'],
+                    'image_url' => $film['image_url'],
+                    'genre_ids' => $film['genre_ids']
+                ] + $validatedFilmData // Fusion des données validées avec les données de l'API
+            );
+        }
+
+        // Récupérer les genres de films depuis l'API TMDB
+        $genresFromApi = $tmdbService->getMovieGenres();
+
+        // Vérifier si aucun genre n'a été récupéré depuis l'API
+        if (empty($genresFromApi)) {
+            return false;
+        }
+
+        // Enregistrer les genres récupérés depuis l'API dans la base de données
+        foreach ($genresFromApi as $genre) {
+            $existingGenre = Genre::where('genre_id', $genre['genre_id'])->first();
+
+            // Si le genre n'existe pas déjà, le créer
+            if (!$existingGenre) {
+                $newGenre = new Genre();
+                $newGenre->name = $genre['name']; // Assigner la valeur du nom récupéré de l'API
+                $newGenre->genre_id = $genre['genre_id']; // Assigner la valeur du genre_id récupéré de l'API
+                $newGenre->fill($validatedGenreData); // Utilisation des données validées pour créer un nouveau genre
+                $newGenre->save();
+            }
+        }
+
+        // Rediriger vers la page d'index des films avec un message de succès
+        return redirect()->route('films.index')->with('success', 'Films récupérés depuis l\'API et enregistrés avec succès.');
+    } catch (\Exception $e) {
+        // Gérer les exceptions
+        return redirect()->back()->with('error', 'Une erreur s\'est produite : ' . $e->getMessage());
     }
-
-    // Récupérer les genres de films depuis l'API TMDB
-    $genresFromApi = $tmdbService->getMovieGenres();
-
-    if (empty($genresFromApi)) {
-        return false;
-    }
-
-// Enregistrer les genres récupérés depuis l'API dans la base de données
-foreach ($genresFromApi as $genre) {
-    $existingGenre = Genre::where('genre_id', $genre['genre_id'])->first();
-
-    if (!$existingGenre) {
-        $newGenre = new Genre();
-        $newGenre->name = $genre['name']; // Assigner la valeur du nom récupéré de l'API
-        $newGenre->genre_id = $genre['genre_id']; // Assigner la valeur du genre_id récupéré de l'API
-        $newGenre->fill($validatedGenreData); // Utilisation des données validées pour créer un nouveau genre
-        $newGenre->save();
-    }
-}
-
-    return redirect()->route('films.index')->with('success', 'Films récupérés depuis l\'API et enregistrés avec succès.');
 }
 
     /**
--- app/Services/TMDBService.php
+++ app/Services/TMDBService.php
@@ -15,45 +15,62 @@ class TMDBService
      */
     public function getTrendingMovies(): array
     {
-        // Création d'un client Guzzle pour effectuer les requêtes HTTP
-        $client = new Client();
-
-        // Effectuer une requête GET à l'API TMDB pour les films populaires
-        $response = $client->request('GET', 'https://api.themoviedb.org/3/trending/all/day?language=en', [
-            'headers' => [
-                'Authorization' => 'Bearer ' . Config::get('tmdb.api_key'),
-                'accept' => 'application/json',
-            ],
-        ]);
-
-        // Décodage de la réponse JSON en tableau associatif
-        $data = json_decode($response->getBody(), true);
-
-        $movies = [];
-        foreach ($data['results'] as $movie) {
-            // Vérifier si le film existe déjà dans la base de données locale
-            $existingMovie = Film::where('movie_id', $movie['id'])->first();
-
-            if (!$existingMovie) {
-                // Créer un nouveau film s'il n'existe pas déjà
-                $newMovie = new Film();
-                $newMovie->movie_id = $movie['id'];
-                $newMovie->title = $movie['title'] ?? $movie['original_title'] ?? $movie['name'] ?? $movie['original_name'] ?? ''; //Parfois, le titre n'existe pas. Pour éviter cela, on peut utiliser le nom original ou le titre original.
-                $newMovie->description = isset($movie['overview']) ? $movie['overview'] : '';
-                $newMovie->image_url = isset($movie['poster_path']) ? 'https://image.tmdb.org/t/p/w500' . $movie['poster_path'] : '';
-                // Convertir le tableau genre_ids en JSON avant l'insertion
-                $newMovie->genre_ids = isset($movie['genre_ids']) && is_array($movie['genre_ids']) ? json_encode($movie['genre_ids']) : '[]';
-                $newMovie->save();
-                $movies[] = $newMovie;
-            } else {
-                // Ajouter le film existant au tableau
-                $existingMovie->genre_ids = json_decode($existingMovie->genre_ids, true);
-                $movies[] = $existingMovie;
+        try {
+            // Création d'un client Guzzle pour effectuer les requêtes HTTP
+            $client = new Client();
+    
+            // Effectuer une requête GET à l'API TMDB pour les films populaires
+            $response = $client->request('GET', 'https://api.themoviedb.org/3/trending/all/day?language=en', [
+                'headers' => [
+                    'Authorization' => 'Bearer ' . Config::get('tmdb.api_key'),
+                    'accept' => 'application/json',
+                ],
+            ]);
+    
+            // Vérifier si la réponse est valide (code HTTP 200)
+            if ($response->getStatusCode() !== 200) {
+                throw new \Exception('Erreur lors de la récupération des films populaires depuis l\'API.');
             }
+    
+            // Décodage de la réponse JSON en tableau associatif
+            $data = json_decode($response->getBody(), true);
+    
+            $movies = [];
+            // Vérifier si des résultats sont retournés dans la réponse
+            if (isset($data['results'])) {
+                foreach ($data['results'] as $movie) {
+                    // Vérifier si le film existe déjà dans la base de données locale
+                    $existingMovie = Film::where('movie_id', $movie['id'])->first();
+    
+                    if (!$existingMovie) {
+                        // Créer un nouveau film s'il n'existe pas déjà
+                        $newMovie = new Film();
+                        $newMovie->movie_id = $movie['id'];
+                        $newMovie->title = $movie['title'] ?? $movie['original_title'] ?? $movie['name'] ?? $movie['original_name'] ?? ''; //Parfois, le titre n'existe pas. Pour éviter cela, on peut utiliser le nom original ou le titre original.
+                        $newMovie->description = isset($movie['overview']) ? $movie['overview'] : '';
+                        $newMovie->image_url = isset($movie['poster_path']) ? 'https://image.tmdb.org/t/p/w500' . $movie['poster_path'] : '';
+                        // Convertir le tableau genre_ids en JSON avant l'insertion
+                        $newMovie->genre_ids = isset($movie['genre_ids']) && is_array($movie['genre_ids']) ? json_encode($movie['genre_ids']) : '[]';
+                        $newMovie->save();
+                        $movies[] = $newMovie;
+                    } else {
+                        // Ajouter le film existant au tableau
+                        $existingMovie->genre_ids = json_decode($existingMovie->genre_ids, true);
+                        $movies[] = $existingMovie;
+                    }
+                }
+            }
+    
+            return $movies; // Retourner la liste des films
+        } catch (\Exception $e) {
+            // Gérer toutes les exceptions qui se produisent pendant la requête
+            // Loguer l'erreur pour un suivi ultérieur
+            \Log::error('Erreur lors de la récupération des films populaires depuis l\'API: ' . $e->getMessage());
+            // Retourner un tableau vide en cas d'erreur
+            return [];
         }
-
-        return $movies;
     }
+    
 
     /**
      * Récupère les genres de films depuis l'API TMDB.
@@ -64,7 +81,7 @@ class TMDBService
     {
         // Création d'un client Guzzle pour effectuer les requêtes HTTP
         $allGenres = new Client();
-
+    
         try {
             // Effectuer une requête GET à l'API TMDB pour les genres de films
             $response_genres = $allGenres->request('GET', 'https://api.themoviedb.org/3/genre/movie/list?language=en', [
@@ -73,29 +90,37 @@ class TMDBService
                     'accept' => 'application/json',
                 ],
             ]);
-
+    
+            // Vérifier si la réponse est valide (code HTTP 200)
+            if ($response_genres->getStatusCode() !== 200) {
+                throw new \Exception('Erreur lors de la récupération des genres de films depuis l\'API.');
+            }
+    
             // Décodage de la réponse JSON en tableau associatif
             $data_genres = json_decode($response_genres->getBody(), true);
-
-            $genres = [];
-            // Vérifier si la réponse contient des genres
-            if (isset($data_genres['genres'])) {
-                foreach ($data_genres['genres'] as $genre) {
-                    // Ajouter chaque genre au tableau
-                    $genres[] = [
-                        'genre_id' => $genre['id'],
-                        'name' => $genre['name']
-                    ];
-                }
-            } else {
-                // Gérer le cas où aucun genre n'est trouvé
-                return [];
+    
+            // Vérifier si des genres ont été retournés dans la réponse
+            if (!isset($data_genres['genres'])) {
+                throw new \Exception('Aucun genre de film trouvé dans la réponse de l\'API.');
             }
-
-            return $genres;
+    
+            $genres = [];
+            // Ajouter chaque genre au tableau
+            foreach ($data_genres['genres'] as $genre) {
+                $genres[] = [
+                    'genre_id' => $genre['id'],
+                    'name' => $genre['name']
+                ];
+            }
+    
+            return $genres; // Retourner la liste des genres
         } catch (\Exception $e) {
             // Gérer toutes les exceptions qui se produisent pendant la requête
+            // Loguer l'erreur pour un suivi ultérieur
+            \Log::error('Erreur lors de la récupération des genres de films depuis l\'API: ' . $e->getMessage());
+            // Retourner un tableau vide en cas d'erreur
             return [];
         }
     }
+    
 }
