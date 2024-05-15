31737d00afc1d130d91933156487f893206ca93e
salaheddine es-sbai <es.sbai.ma@gmail.com>
2024-05-15 17:38:54 +0100

    Custom request FilmRequest

---
+++ app/Http/Requests/FilmRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class FilmRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|string|max:255',
+            'movie_id' => 'required|integer',
+            'description' => 'required|string',
+            'image_url' => 'required|url',
+            'genre_ids' => 'required|array',
+        ];
+    }
+}
